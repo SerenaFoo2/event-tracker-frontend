a1577c0c5f2ee3572ec214279b2568d322ec8466
Hong Yuan <loohongyuan5505@hotmail.com>
2022-12-22 21:22:52 +0800

    remove unuse Colors in navigationBar

--- src/styles/Navigationbar/index.jsx
+++ src/styles/Navigationbar/index.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { styled, Toolbar, Box, Typography } from "@mui/material";
-import { Colors } from "../theme";
 
 export const NavBarLink = styled(Link)({
   textDecoration: "none",
